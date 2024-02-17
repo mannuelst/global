a42d6756a239e9e5cc9342ff662a5c280dd05c53
Manuel dos Santos <manuelds@outlook.pt>
2024-02-17 19:01:34 +0100

    create the header

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="bg-gray-100 min-h-screen">
+          <nav className="w-full bg-white h-16 flex items-center justify-center">
+            <section className="container flex items-center">
+              <h1 className="font-bold text-2xl">
+                Lista de Países
+              </h1>
+            </section>
+          </nav>
+          {children}
+
+        </main>
+      </body>
     </html>
   );
 }
