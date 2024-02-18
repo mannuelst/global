801e81e7fbc7acd5225f188e0580c10d8a1d1f69
Manuel dos Santos <manuelds@outlook.pt>
2024-02-18 19:01:54 +0100

    update on title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "kountries",
+  title: "global",
   description: "List of countries",
 };
 
