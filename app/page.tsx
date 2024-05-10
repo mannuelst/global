fb0bf52af8971126103af6a9a5bef95606920283
Manuel dos Santos <manuelds@outlook.pt>
2024-05-10 20:13:29 +0100

    feat: translation pt

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-
-async function getCountries() {
+type Country = {
+  name: {
+    common: string
+  }
+}
+async function getCountries(): Promise<Country[]> {
   const response = fetch("https://restcountries.com/v3.1/all")
   return (await response).json()
 
@@ -10,10 +14,13 @@ export default async function Home() {
   const countries = await getCountries()
 
   return (
-    <section className="flex container w-full">
+    <section className="grid grid-cols-5 container w-full gap-2 mt-16 ">
       {
         countries.map((country) => (
-          <h1 key={country.name.common}>{country.name.common}</h1>
+          <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl" key={country.name.common}>
+            <h1>{country.translations.por.common}</h1>
+
+          </article>
         ))
       }
     </section>
