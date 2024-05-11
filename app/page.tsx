bd6ecf2875ab0c2409b982378c5405835248096c
Manuel dos Santos <manuelds@outlook.pt>
2024-05-11 20:33:41 +0100

    feat: add hover-effect

--- app/page.tsx
+++ app/page.tsx
@@ -28,12 +28,12 @@ export default async function Home() {
     <section className="grid grid-cols-5 container w-full gap-2 mt-16 ">
       {
         countries.map((country) => (
-          <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl" key={country.name.common}>
+          <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 hover:shadow-xl transition-all" key={country.name.common}>
             <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
 
               <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover" />
             </div>
-            <h1>{country.translations.por.common}</h1>
+            <h1 className="font-bold text-xl text-center mt-1">{country.translations.por.common}</h1>
 
           </article>
         ))
