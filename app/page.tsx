e3597ce180daf67f53cbb0b7533d99c7624285b5
Manuel dos Santos <manuelds@outlook.pt>
2024-05-11 20:38:38 +0100

    feat: country-route

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 type Country = {
   name: {
@@ -28,14 +29,16 @@ export default async function Home() {
     <section className="grid grid-cols-5 container w-full gap-2 mt-16 ">
       {
         countries.map((country) => (
-          <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 hover:shadow-xl transition-all" key={country.name.common}>
-            <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
+          <Link href={`/country/${country.name.common}`}>
+            <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 hover:shadow-xl transition-all" key={country.name.common}>
+              <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
 
-              <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover" />
-            </div>
-            <h1 className="font-bold text-xl text-center mt-1">{country.translations.por.common}</h1>
+                <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover" />
+              </div>
+              <h1 className="font-bold text-xl text-center mt-1">{country.translations.por.common}</h1>
 
-          </article>
+            </article>
+          </Link>
         ))
       }
     </section>
